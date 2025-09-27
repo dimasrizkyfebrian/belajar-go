0d913164e2ea597f12d540d1f834ad4c01f50c90
Dimas Rizky Febrian <dimasrfebrian@gmail.com>
2025-09-27 14:48:25 +0700

    Modul 1: Belajar Konstanta dan Tipe Data Numerik

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	namaLengkap := "Dimas Rizky Febrian"
-	pekerjaan := "Calon Go Developer"
-	umur := 22
-	kotaAsal := "Bekasi"
-	apakahSudahMakan := true
+	const namaAplikasi = "Aplikasi Belajar Go"
+	const versi = "1.0"
+	const asalNegara = "Indonesia"
 
-	fmt.Println("Halo, nama lengkap saya:")
-	fmt.Println(namaLengkap)
+	var umur int = 22
+	var tinggiBadan float64 = 172.5
+	var nomorAbsen uint = 10
 
-	fmt.Println("Pekerjaan saya adalah:")
-	fmt.Println(pekerjaan)
+	fmt.Println("Selamat datang di", namaAplikasi, "versi", versi)
+	fmt.Println("===============================================")
 
-	fmt.Println("Saya berasal dari kota", kotaAsal)
-	fmt.Println("Umur saya:", umur, "tahun")
-	fmt.Println("Sudah makan?", apakahSudahMakan)
+	fmt.Printf("Umur saya: %d tahun\n", umur)
+	fmt.Printf("Nomor absen saya adalah: %d\n", nomorAbsen)
+	fmt.Printf("Tinggi badan saya adalah %.1f cm\n", tinggiBadan)
+	fmt.Printf("Saya berasal dari negara %s\n", asalNegara)
 }
