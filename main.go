26db9525376db518429ec295eeb9ebb101cf57a4
Dimas Rizky Febrian <dimasrfebrian@gmail.com>
2025-09-27 14:29:42 +0700

    Modul 1: Belajar Variabel dan Tipe Data

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Halo, Dunia! Saya siap belajar Go!")
+	namaLengkap := "Dimas Rizky Febrian"
+	pekerjaan := "Calon Go Developer"
+	umur := 22
+	kotaAsal := "Bekasi"
+	apakahSudahMakan := true
+
+	fmt.Println("Halo, nama lengkap saya:")
+	fmt.Println(namaLengkap)
+
+	fmt.Println("Pekerjaan saya adalah:")
+	fmt.Println(pekerjaan)
+
+	fmt.Println("Saya berasal dari kota", kotaAsal)
+	fmt.Println("Umur saya:", umur, "tahun")
+	fmt.Println("Sudah makan?", apakahSudahMakan)
 }
