6d41a24e4fc4fc26262a35e5826d822aad20a3e6
Dimas Rizky Febrian <dimasrfebrian@gmail.com>
2025-09-27 17:59:21 +0700

    Modul 2: Belajar Percabangan dengan switch

--- Modul2/main.go
+++ Modul2/main.go
@@ -3,31 +3,54 @@ package main
 import "fmt"
 
 func main() {
-	nilaiUjian := 96
+	char := 'z'
 
-	fmt.Printf("Nilai ujian Anda adalah: %d\n", nilaiUjian)
+	switch char {
+	case 'a', 'i', 'u', 'e', 'o':
+		fmt.Printf("'%c' adalah huruf vokal.\n", char)
+	default:
+		fmt.Printf("'%c' adalah huruf konsonan.\n", char)
+	}
 
-	if nilaiUjian >= 90 {
+	fmt.Println("==================================")
+
+	nilaiUjian := 68
+
+	fmt.Printf("Nilai ujian Anda: %d\n", nilaiUjian)
+
+	switch {
+	case nilaiUjian >= 90:
 		fmt.Println("Predikat: A (Luar Biasa!)")
-	} else if nilaiUjian >= 80 {
+	case nilaiUjian >= 80:
 		fmt.Println("Predikat: B (Bagus)")
-	} else if nilaiUjian >= 70 {
+	case nilaiUjian >= 70:
 		fmt.Println("Predikat: C (Cukup)")
-	} else {
+	default:
 		fmt.Println("Predikat: D (Perlu belajar lagi)")
 	}
 
 	fmt.Println("==================================")
 
-	angka := -1
+	nomorHari := 8
 
-	fmt.Printf("Angka adalah: %d\n", angka)
+	fmt.Printf("Nomor hari: %d\n", nomorHari)
 
-	if angka > 0 {
-		fmt.Printf("%d adalah angka positif.\n", angka)
-	} else if angka < 0 {
-		fmt.Printf("%d adalah angka negatif.\n", angka)
-	} else {
-		fmt.Printf("Angka adalah nol.\n")
+	switch nomorHari {
+	case 1:
+		fmt.Println("Senin")
+	case 2:
+		fmt.Println("Selasa")
+	case 3:
+		fmt.Println("Rabu")
+	case 4:
+		fmt.Println("Kamis")
+	case 5:
+		fmt.Println("Jumat")
+	case 6:
+		fmt.Println("Sabtu")
+	case 7:
+		fmt.Println("Minggu")
+	default:
+		fmt.Println("Nomor hari tidak valid")
 	}
 }
